d11147b77205acca1061ccec8e261f362168f42c
sreehari333 <85100069+sreehari333@users.noreply.github.com>
2021-06-01 13:42:25 +0530

    Add files via upload

---
+++ index value of an array.py
@@ -0,0 +1,7 @@
+my_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+
+print("The index of element C is ", my_list.index('C', 1, 5))
+
+print("The index of element F is ", my_list.index('F', 3, 7))
+
+print("The index of element D is ", my_list.index('D', 1))
